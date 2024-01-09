33896d863355e753717ec3cdb88137447bf3bea5
Abhindas <abhindasofficial2@gmail.com>
2024-01-09 16:25:59 +0530

    second page addition

---
+++ index.js
@@ -0,0 +1,9 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+    setTimeout(closeModal, 1000); 
+  }
+
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+
+  }
